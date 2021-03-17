4a2012289d12f16b978a314534fb85aa2cfc168a
jgalgarra <jgalgarra@coit.es>
2021-03-17 10:27:16 +0100

    Submission rearrange

--- simplexML/Pyscripts/rse.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Auxiliar function to compute Root Squared Error
-"""
-import numpy as np
-
-def calc_rse(valores,prediccion):
-    return(sum((valores-prediccion)**2)/sum((valores-np.mean(valores))**2))
